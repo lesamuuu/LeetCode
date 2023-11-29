3043473db34887c4803d6fbf003971dfc59fa0f0
LeSamuuu <samuelpayback@gmail.com>
2023-11-29 00:22:22 +0000

    HubKeys used to store global constants

---
+++ ExercisesHub/HubKeys.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeRunner.ExercisesHub
+{
+    public static class HubKeys
+    {
+        public static class FolderPaths
+        {
+            public const string ProblemsFolder = @"A:\Programas\JetBrains\RidersProjects\LeetCode\LeetCode\Problems";
+            public const string EasyFolder = @"A:\Programas\JetBrains\RidersProjects\LeetCode\LeetCode\Problems\Easy";
+            public const string MediumFolder = @"A:\Programas\JetBrains\RidersProjects\LeetCode\LeetCode\Problems\Medium";
+            public const string HardFolder = @"A:\Programas\JetBrains\RidersProjects\LeetCode\LeetCode\Problems\Hard";
+        }
+        
+        public static class Dialogs
+        {
+            public const string AskForExerciseNumber = "What exercise do you want to execute?";
+        }
+    }
+}
